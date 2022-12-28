975f00a72c06806be4fe379054a2cbd430c30cd4
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-12-28 13:30:38 -0300

    copyToClipBoard incremented

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,16 @@
 window.onload = function () {
   let criptButton = document.querySelector("#crypt-button");
-  criptButton.onclick = getText;
+  criptButton.onclick = encryptText;
   let copyButton = document.querySelector("#copy-button");
   copyButton.onclick = copyToClipBoard;
+  let decryptButton = document.querySelector("#decrypt-button");
+  decryptButton.onclick = decryptText;
 };
 
-function getText() {
+function encryptText() {
   let text = document.querySelector("#text-area").value;
-  if (text != "" && !text.match(/[\d]/i)) {
+  text = encrypt(text);
+  if (text != "" && !text.match(/[\d]/gim)) {
     document.querySelector("#text-area").value = "";
     document.querySelector("#message-board-warning").style.visibility =
       "hidden";
@@ -18,11 +21,39 @@ function getText() {
   }
 }
 
+function decryptText() {
+  let decryptText = document.querySelector("#text-area").value;
+  if (decryptText != "" && !decryptText.match(/[\d]/gim)) {
+    document.querySelector("#text-area").value = "";
+    document.querySelector("#message-board-warning").style.visibility =
+      "hidden";
+    document.querySelector("#message-board-previous").style.visibility =
+      "visible";
+      decryptText = decrypt(decryptText);
+
+    document.getElementById("message-board-text-area").value = decryptText;
+  }
+}
+
 function copyToClipBoard() {
-  let copyText = document.querySelector("#message-board-text-area"); 
+  let copyText = document.querySelector("#message-board-text-area");
 
-  navigator.clipboard.writeText(copyText.value);
-
+  navigator.clipboard.write(copyText);
+  document.querySelector("#text-area").value = copyText.value;
+  document.querySelector("#message-board-text-area").value = "";
   alert("Copied the text: " + copyText.value);
+}
 
+function encrypt(text) {
+  text = text.replaceAll("e", "enter");
+  text = text.replaceAll("i", "imes");
+  text = text.replaceAll("a", "ai");
+  text = text.replaceAll("o", "ober");
+  text = text.replaceAll("u", "ufat");
+  return text;
+}
+
+function decrypt(decryptText) {
+    decryptText.replaceAll("enter", "e");
+    return decryptText;
 }
